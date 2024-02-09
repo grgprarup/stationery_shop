5ab96ec1f2641841e7ab489606359e3bc27a168b
Prarup Gurung <grgprarup@gmail.com>
2024-02-09 22:28:22 +0545

    Removed template code from main file and added icons, styles for login page

--- lib/src/auth/login_screen.dart
+++ lib/src/auth/login_screen.dart
@@ -1,61 +1,131 @@
 import 'package:flutter/material.dart';
 import 'package:stationery_shop/src/auth/register_screen.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool _isPasswordVisible = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      ),
+      // backgroundColor: const Color(0xFF19B0E7),
       body: Padding(
-        padding: EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
+            const Text(
+              'Login',
+              style: TextStyle(
+                fontSize: 26.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Text(
+              'Please sign in to continue.',
+              style: TextStyle(
+                fontSize: 14.0,
+                fontWeight: FontWeight.normal,
+                color: Color(0xFF7D7D7D),
+              ),
+            ),
+            const SizedBox(height: 48.0),
             TextFormField(
               decoration: const InputDecoration(
                 labelText: 'Username',
                 border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.person),
               ),
             ),
             const SizedBox(height: 16.0),
             TextFormField(
-              decoration: const InputDecoration(
+              decoration: InputDecoration(
                 labelText: 'Password',
-                border: OutlineInputBorder(),
+                border: const OutlineInputBorder(),
+                prefixIcon: const Icon(Icons.lock),
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    _isPasswordVisible
+                        ? Icons.visibility
+                        : Icons.visibility_off,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      _isPasswordVisible = !_isPasswordVisible;
+                    });
+                  },
+                ),
               ),
-              obscureText: true,
+              obscureText: !_isPasswordVisible,
             ),
             const SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => const RegisterScreen()));
-              },
-              child: const Text('Login'),
+            Align(
+              alignment: Alignment.centerRight,
+              child: FractionallySizedBox(
+                widthFactor: 0.35,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // TODO: Implement login functionality
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                      const Color(0xFF19B0E7),
+                    ),
+                    foregroundColor: MaterialStateProperty.all<Color>(
+                      Colors.white,
+                    ),
+                  ),
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('LOGIN'),
+                      Icon(Icons.arrow_forward),
+                    ],
+                  ),
+                ),
+              ),
             ),
             const SizedBox(height: 8.0),
             TextButton(
-              onPressed: () {},
-              child: const Text('Forgot Password'),
+              onPressed: () {
+                // TODO: Implement forgot password functionality
+              },
+              child: const Text(
+                'Forgot Password?',
+                style: TextStyle(
+                  color: Color(0xFF19B0E7),
+                ),
+              ),
             ),
-            const SizedBox(height: 8.0),
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-              const Text('Don\'t have an account?'),
-              TextButton(
-                onPressed: () {
-                  Navigator.push(
+            const SizedBox(height: 6.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('Don\'t have an account?'),
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(
                       context,
                       MaterialPageRoute(
-                          builder: (context) => const RegisterScreen()));
-                },
-                child: const Text('Register'),
-              ),
-            ])
+                          builder: (context) => const RegisterScreen()),
+                    );
+                  },
+                  child: const Text(
+                    'Sign up',
+                    style: TextStyle(
+                      color: Color(0xFF19B0E7),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
