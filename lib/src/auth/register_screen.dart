a3d23d910ce88d63524e76f93d6a44dd9f83bc30
Prarup Gurung <grgprarup@gmail.com>
2024-02-09 23:06:30 +0545

    Sign up page implemented

--- lib/src/auth/register_screen.dart
+++ lib/src/auth/register_screen.dart
@@ -1,16 +1,173 @@
 import 'package:flutter/material.dart';
 
-class RegisterScreen extends StatelessWidget {
+import 'login_screen.dart';
+
+class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
+
+  @override
+  _RegisterScreenState createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  bool _isPasswordVisible = false;
+  bool _isConfirmPasswordVisible = false;
+  bool _isLongPressed = false;
+  bool _isConfirmLongPressed = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Register'),
+        title: null,
       ),
-      body: const Center(
-        child: Text('Register Screen'),
+      // backgroundColor: const Color(0xFF19B0E7),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Text(
+              'Create an Account',
+              style: TextStyle(
+                fontSize: 26.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 48.0),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Full Name',
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.person_2),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Username',
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.person_outline),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Password',
+                border: const OutlineInputBorder(),
+                prefixIcon: const Icon(Icons.lock),
+                suffixIcon: GestureDetector(
+                  onLongPress: () {
+                    setState(() {
+                      _isLongPressed = true;
+                    });
+                  },
+                  onLongPressUp: () {
+                    setState(() {
+                      _isLongPressed = false;
+                    });
+                  },
+                  child: IconButton(
+                    icon: Icon(
+                      _isPasswordVisible || _isLongPressed
+                          ? Icons.visibility
+                          : Icons.visibility_off,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _isPasswordVisible = !_isPasswordVisible;
+                      });
+                    },
+                  ),
+                ),
+              ),
+              obscureText: !_isPasswordVisible && !_isLongPressed,
+            ),
+            const SizedBox(height: 16.0),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Confirm Password',
+                border: const OutlineInputBorder(),
+                prefixIcon: const Icon(Icons.lock),
+                suffixIcon: GestureDetector(
+                  onLongPress: () {
+                    setState(() {
+                      _isConfirmLongPressed = true;
+                    });
+                  },
+                  onLongPressUp: () {
+                    setState(() {
+                      _isConfirmLongPressed = false;
+                    });
+                  },
+                  child: IconButton(
+                    icon: Icon(
+                      _isConfirmPasswordVisible || _isConfirmLongPressed
+                          ? Icons.visibility
+                          : Icons.visibility_off,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _isConfirmPasswordVisible = !_isConfirmPasswordVisible;
+                      });
+                    },
+                  ),
+                ),
+              ),
+              obscureText: !_isConfirmPasswordVisible && !_isConfirmLongPressed,
+            ),
+            const SizedBox(height: 16.0),
+            Align(
+              alignment: Alignment.centerRight,
+              child: FractionallySizedBox(
+                widthFactor: 0.35,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // TODO: Implement registration functionality
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                      const Color(0xFF19B0E7),
+                    ),
+                    foregroundColor: MaterialStateProperty.all<Color>(
+                      Colors.white,
+                    ),
+                  ),
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('SIGN UP'),
+                      Icon(Icons.arrow_forward),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 8.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('Already have an account?'),
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const LoginScreen()),
+                    );
+                  },
+                  child: const Text(
+                    'Sign in',
+                    style: TextStyle(
+                      color: Color(0xFF19B0E7),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
