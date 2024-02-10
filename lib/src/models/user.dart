814c01b0bae01039b9f65ffcf9ffa73c4e20de68
Prarup Gurung <grgprarup@gmail.com>
2024-02-10 14:21:49 +0545

    Added user model

---
+++ lib/src/models/user.dart
@@ -0,0 +1,22 @@
+class User {
+  final String fullName;
+  final String username;
+  final String password;
+  final String confirmPassword;
+
+  User({
+    required this.fullName,
+    required this.username,
+    required this.password,
+    required this.confirmPassword,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'full_name': fullName,
+      'username': username,
+      'password': password,
+      'confirm_password': confirmPassword,
+    };
+  }
+}
