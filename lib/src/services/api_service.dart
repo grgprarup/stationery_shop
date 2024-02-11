91f96d7ed209e38f3d94c39934890ed35fb9c394
Prarup Gurung <grgprarup@gmail.com>
2024-02-11 17:34:18 +0545

    Optimized ApiService and added _makeRequest method to separate request logic

--- lib/src/services/api_service.dart
+++ lib/src/services/api_service.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../models/user.dart';
+import '../utils/http_helper.dart';
 
 class ApiResponse {
   final bool success;
@@ -17,23 +18,40 @@ class ApiService {
   Future<ApiResponse> registerUser(User user) async {
     bool isSuccess = false;
     try {
-      final response = await http.post(
-        Uri.parse('$baseUrl/users/'),
-        headers: {'Content-Type': 'application/json'},
-        body: jsonEncode(user.toJson()),
-      );
-      final responseMessage = (json.decode(response.body))['message'].toString();
+      final response =
+          await _makeRequest(baseUrl, 'users/', 'POST', user.toJson(), null);
+
+      final responseMessage =
+          (json.decode(response.body))['message'].toString();
 
       if (response.statusCode == 201) {
         isSuccess = true;
-        return ApiResponse(
-            success: isSuccess, message: responseMessage);
-      } else {
-        return ApiResponse(
-            success: isSuccess, message: responseMessage);
       }
+      return ApiResponse(success: isSuccess, message: responseMessage);
     } catch (e) {
       return ApiResponse(success: isSuccess, message: e.toString());
+    }
+  }
+
+  Future<http.Response> _makeRequest(String baseUrl, String endpoint,
+      String method, Object? body, Encoding? encoding) async {
+    final httpHelper = HttpHelper(
+        fullUrl: '$baseUrl/$endpoint',
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode(body),
+        encoding: encoding);
+
+    switch (method) {
+      case 'GET':
+        return await httpHelper.get();
+      case 'POST':
+        return await httpHelper.post();
+      case 'PUT':
+        return await httpHelper.put();
+      case 'DELETE':
+        return await httpHelper.delete();
+      default:
+        throw Exception('Invalid Method: $method');
     }
   }
 }
