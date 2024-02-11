d0e8fd2a5de81050f4610e8d992f2e86907dad88
Prarup Gurung <grgprarup@gmail.com>
2024-02-11 18:36:12 +0545

    Scroll view added for register screen, design related changes done

--- lib/src/screens/register_screen.dart
+++ lib/src/screens/register_screen.dart
@@ -70,146 +70,152 @@ class _RegisterScreenState extends State<RegisterScreen> {
         title: null,
       ),
       // backgroundColor: const Color(0xFF19B0E7),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const Text(
-              'Create an Account',
-              style: TextStyle(
-                fontSize: 26.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 48.0),
-            TextFormField(
-              controller: _fullNameController,
-              decoration: const InputDecoration(
-                labelText: 'Full Name',
-                border: OutlineInputBorder(),
-                prefixIcon: Icon(Icons.person_2),
-              ),
-            ),
-            const SizedBox(height: 16.0),
-            TextFormField(
-              controller: _usernameController,
-              decoration: const InputDecoration(
-                labelText: 'Username',
-                border: OutlineInputBorder(),
-                prefixIcon: Icon(Icons.person_outline),
-              ),
-            ),
-            const SizedBox(height: 16.0),
-            TextFormField(
-              controller: _passwordController,
-              decoration: InputDecoration(
-                labelText: 'Password',
-                border: const OutlineInputBorder(),
-                prefixIcon: const Icon(Icons.lock),
-                suffixIcon: GestureDetector(
-                  onLongPress: () {
-                    setState(() {
-                      _isPasswordVisible = true;
-                    });
-                  },
-                  onLongPressUp: () {
-                    setState(() {
-                      _isPasswordVisible = false;
-                    });
-                  },
-                  child: IconButton(
-                    icon: Icon(
-                      _isPasswordVisible
-                          ? Icons.visibility
-                          : Icons.visibility_off,
-                    ),
-                    onPressed: () {},
-                  ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Text(
+                'Create an Account',
+                style: TextStyle(
+                  fontSize: 32.0,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-              obscureText: !_isPasswordVisible,
-            ),
-            const SizedBox(height: 16.0),
-            TextFormField(
-              controller: _confirmPasswordController,
-              decoration: InputDecoration(
-                labelText: 'Confirm Password',
-                border: const OutlineInputBorder(),
-                prefixIcon: const Icon(Icons.lock),
-                suffixIcon: GestureDetector(
-                  onLongPress: () {
-                    setState(() {
-                      _isConfirmPasswordVisible = true;
-                    });
-                  },
-                  onLongPressUp: () {
-                    setState(() {
-                      _isConfirmPasswordVisible = false;
-                    });
-                  },
-                  child: IconButton(
-                    icon: Icon(
-                      _isConfirmPasswordVisible
-                          ? Icons.visibility
-                          : Icons.visibility_off,
-                    ),
-                    onPressed: () {},
-                  ),
+              const SizedBox(height: 48.0),
+              TextFormField(
+                controller: _fullNameController,
+                decoration: const InputDecoration(
+                  labelText: 'Full Name',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.person_2),
                 ),
               ),
-              obscureText: !_isConfirmPasswordVisible,
-            ),
-            const SizedBox(height: 16.0),
-            Align(
-              alignment: Alignment.centerRight,
-              child: FractionallySizedBox(
-                widthFactor: 0.35,
-                child: ElevatedButton(
-                  onPressed: () => _onRegistrationSuccess(context),
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(
-                      const Color(0xFF19B0E7),
-                    ),
-                    foregroundColor: MaterialStateProperty.all<Color>(
-                      Colors.white,
-                    ),
-                  ),
-                  child: const Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text('SIGN UP'),
-                      Icon(Icons.arrow_forward),
-                    ],
-                  ),
+              const SizedBox(height: 16.0),
+              TextFormField(
+                controller: _usernameController,
+                decoration: const InputDecoration(
+                  labelText: 'Username',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.person_outline),
                 ),
               ),
-            ),
-            const SizedBox(height: 8.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text('Already have an account?'),
-                TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) =>
-                              LoginScreen(apiService: apiService)),
-                    );
-                  },
-                  child: const Text(
-                    'Sign in',
-                    style: TextStyle(
-                      color: Color(0xFF19B0E7),
+              const SizedBox(height: 16.0),
+              TextFormField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: const OutlineInputBorder(),
+                  prefixIcon: const Icon(Icons.lock),
+                  suffixIcon: GestureDetector(
+                    onLongPress: () {
+                      setState(() {
+                        _isPasswordVisible = true;
+                      });
+                    },
+                    onLongPressUp: () {
+                      setState(() {
+                        _isPasswordVisible = false;
+                      });
+                    },
+                    child: IconButton(
+                      icon: Icon(
+                        _isPasswordVisible
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                      ),
+                      onPressed: () {},
                     ),
                   ),
                 ),
-              ],
-            ),
-          ],
+                obscureText: !_isPasswordVisible,
+              ),
+              const SizedBox(height: 16.0),
+              TextFormField(
+                controller: _confirmPasswordController,
+                decoration: InputDecoration(
+                  labelText: 'Confirm Password',
+                  border: const OutlineInputBorder(),
+                  prefixIcon: const Icon(Icons.lock),
+                  suffixIcon: GestureDetector(
+                    onLongPress: () {
+                      setState(() {
+                        _isConfirmPasswordVisible = true;
+                      });
+                    },
+                    onLongPressUp: () {
+                      setState(() {
+                        _isConfirmPasswordVisible = false;
+                      });
+                    },
+                    child: IconButton(
+                      icon: Icon(
+                        _isConfirmPasswordVisible
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                ),
+                obscureText: !_isConfirmPasswordVisible,
+              ),
+              const SizedBox(height: 32.0),
+              Align(
+                alignment: Alignment.centerRight,
+                child: FractionallySizedBox(
+                  widthFactor: 0.35,
+                  child: ElevatedButton(
+                    onPressed: () => _onRegistrationSuccess(context),
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                        const Color(0xFF19B0E7),
+                      ),
+                      foregroundColor: MaterialStateProperty.all<Color>(
+                        Colors.white,
+                      ),
+                    ),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text('SIGN UP'),
+                        Icon(Icons.arrow_forward),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 48.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('Already have an account?',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                      )),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                LoginScreen(apiService: apiService)),
+                      );
+                    },
+                    child: const Text(
+                      'Sign in',
+                      style: TextStyle(
+                        color: Color(0xFF19B0E7),
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
