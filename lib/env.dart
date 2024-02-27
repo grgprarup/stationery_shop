c5789d6c07ee69367913c87c48f7ebd1a1f33cec
Prarup Gurung <grgprarup@gmail.com>
2024-02-28 03:05:50 +0545

    Api host default to localhost

--- lib/env.dart
+++ lib/env.dart
@@ -1,5 +1,5 @@
 class Env {
-  static const String apiBaseUrl = 'http://10.0.2.2';
+  static const String apiBaseUrl = 'http://localhost';
   static const String apiPort = '5000';
   static const String apiPath = 'api';
   static const String apiVersion = 'v1.0';
