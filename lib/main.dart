3d627b5c3eca030727ca380ef294f8e09c16527e
Prarup Gurung <grgprarup@gmail.com>
2024-05-29 17:13:44 +0545

    Changed project app class name

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,13 @@ import '../../env.dart';
 void main() {
   const baseUrl = '${Env.apiBaseUrl}:${Env.apiPort}/${Env.apiPath}/${Env.apiVersion}';
   final apiService = ApiService(baseUrl: baseUrl);
-  runApp(MyApp(apiService: apiService,));
+  runApp(InventoryApp(apiService: apiService,));
 }
 
-class MyApp extends StatelessWidget {
+class InventoryApp extends StatelessWidget {
   final ApiService apiService;
 
-  const MyApp({super.key, required this.apiService});
+  const InventoryApp({super.key, required this.apiService});
 
   // This widget is the root of your application.
   @override
